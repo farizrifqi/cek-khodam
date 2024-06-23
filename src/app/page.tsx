85e5a4b5f4a4611f786fe20a922f04d8097ca832
zeraneL <farizrifqi26@gmail.com>
2024-06-23 11:06:21 +0800

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,153 @@
-import Image from "next/image";
+"use client";
 
+import { useEffect, useState } from "react";
+import kodam from "../lib/kodam.json";
+import { Social } from "@/components/socials";
+
+type resultKodam = {
+  namaOrang?: string;
+  kodam?: string;
+  arti?: string;
+};
+type phase = {
+  status: string;
+};
+
+const khodamPhase: phase[] = [
+  {
+    status: "Mengumpulkan energi...",
+  },
+  {
+    status: "Mencari informasi orang...",
+  },
+  {
+    status: "Menggali leluhur...",
+  },
+  {
+    status: "Mencari informasi kodam...",
+  },
+];
 export default function Home() {
+  const [nama, setNama] = useState("");
+  const [mencariKodam, setMencariKodam] = useState("");
+  const [phase, setPhase] = useState(-1);
+  const [usernameTiktok, setUsernameTiktok] = useState("");
+  const [result, setResult] = useState({} as resultKodam);
+  const [panel, setPanel] = useState(false);
+
+  useEffect(() => {
+    if (phase == -1) return;
+    if (phase > 3) {
+      setPhase(-1);
+    } else {
+      if (phase == 3) {
+        let kodamTerpilih =
+          kodam[Math.floor(Math.random() * kodam.length + 30)] ??
+          kodam[kodam.length - 1];
+        setResult({
+          namaOrang: nama,
+          kodam: kodamTerpilih.name,
+          arti: kodamTerpilih.meaning,
+        });
+        setNama("");
+        setMencariKodam("");
+      } else {
+        setMencariKodam(khodamPhase[phase]?.status ?? "");
+        setTimeout(() => {
+          setPhase((prev) => prev + 1);
+        }, getRandomInt(750, 3000));
+      }
+    }
+  }, [phase]);
+
+  const cekKodam = () => {
+    setPhase(0);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex h-screen min-h-screen flex-col items-center justify-center py-10">
+      {panel ? (
+        <div className="h-full max-h-screen items-center justify-around flex flex-col">
+          <div className="font-extrabold text-center text-2xl lg:text-4xl w-full flex flex-col animate-pulse items-center justify-center text-white">
+            <div className="items-center flex flex-row">
+              <svg
+                viewBox="0 0 448 512"
+                fill="currentColor"
+                height="1.2em"
+                width="1.2em"
+                className="mr-2"
+              >
+                <path d="M448 209.91a210.06 210.06 0 01-122.77-39.25v178.72A162.55 162.55 0 11185 188.31v89.89a74.62 74.62 0 1052.23 71.18V0h88a121.18 121.18 0 001.86 22.17A122.18 122.18 0 00381 102.39a121.43 121.43 0 0067 20.14z" />
+              </svg>
+              {usernameTiktok}
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-6">
+            <h1 className="lg:text-6xl text-3xl font-extrabold text-red-800 uppercase caveat-brush-regular">
+              Cek Khodam
+            </h1>
+            <p className="text-center lg:text-xl font-semibold">
+              Masukkan nama untuk mengecek Khodam
+            </p>
+            <input
+              value={nama}
+              type="text"
+              className="font-semibold tracking-wider bg-opacity-20 bg-black border-2 border-red-800 rounded-lg focus:outline-none outline-none px-3 py-2 text-xl text-red-800 uppercase tracking-wider placeholder:text-black placeholder:text-opacity-40"
+              placeholder="Masukkan nama"
+              onChange={(e) => setNama(e.target.value)}
             />
-          </a>
+            <button
+              onClick={cekKodam}
+              className="border px-2 py-1 bg-900 bg-red-900 transition-all hover:bg-red-950 text-red-500 rounded border-red-800 disabled:bg-red-950 disabled:cursor-not-allowed"
+              disabled={nama === ""}
+            >
+              Cek!!!
+            </button>
+          </div>
+
+          <div className="animate-bounce text-xl lg:text-3xl">
+            {mencariKodam}
+          </div>
+
+          <div
+            className={`${
+              !result.namaOrang && "opacity-0"
+            } flex flex-col text-left self-start text-xl font-semibold border-4 border-black p-5 lg:w-[30vw] m-3 rounded uppercase gap-3`}
+          >
+            <div className="flex flex-col">
+              <b>Nama:</b> {result.namaOrang}
+            </div>
+            <div className="flex flex-col">
+              <b>Khodam:</b> {result.kodam}
+            </div>
+            <div className="flex flex-col">
+              <b>Arti:</b> {result.arti}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ) : (
+        <div className="flex flex-col font-bold text-lg">
+          Input username terlebih dahulu:
+          <input
+            type="text"
+            className="border outline-none rounded-md p-2 my-1"
+            onChange={(e) => setUsernameTiktok(e.target.value)}
+          ></input>
+          <button
+            onClick={() => setPanel(true)}
+            className="border-2 rounded px-3 py-1 my-2 text-black border-black bg-red-800 hover:bg-red-900"
+          >
+            MASUK KE CEK KODAM
+          </button>
+        </div>
+      )}
+      <Social />
     </main>
   );
+}
+
+function getRandomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
---
+++ src/components/socials.tsx
@@ -0,0 +1,29 @@
+import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
+
+export function Social() {
+  return (
+    <div className="flex flex-row text-red-200 items-center mt-10 text-sm gap-3">
+      <a
+        href="https://tiktok.com/@sosokidola"
+        className="flex flex-row items-center gap-0.5 hover:text-red-100 transition-colors"
+      >
+        <Icon icon="simple-icons:tiktok" className="text-xs" />
+        sosokidola
+      </a>
+      <a
+        href="https://instagram.com/@frz.ra"
+        className="flex flex-row items-center gap-0.5 hover:text-red-100 transition-colors"
+      >
+        <Icon icon="akar-icons:instagram-fill" />
+        frz.ra
+      </a>
+      <a
+        href="https://twitter.com/@zeranel"
+        className="flex flex-row items-center gap-0.5 hover:text-red-100 transition-colors"
+      >
+        <Icon icon="akar-icons:x-fill" className="social-link" />
+        zeraneL
+      </a>
+    </div>
+  );
+}
